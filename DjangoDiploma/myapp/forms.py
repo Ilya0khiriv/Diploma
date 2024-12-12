0eda8f71de4f5a8b38d4a43222fdb35185185bc0
Ilya0khiriv <55556801+Ilya0khiriv@users.noreply.github.com>
2024-12-12 14:40:43 +0500

    Delete DjangoDiploma directory

--- DjangoDiploma/myapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class ContactForm(forms.Form):
-    question = forms.CharField(max_length=1000, label='Введите запрос:')
-
-
-class AmountForm(forms.Form):
-    amount = forms.IntegerField(label='Введите запрос:')
--- DjangoDiploma/myapp/urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import path
-from . import views  # Import your app's views
-
-
